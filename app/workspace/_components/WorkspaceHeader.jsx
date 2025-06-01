06ddafa7cc3b2fdceca15f205102bc799c1bd7e8
Arya142004 <aryamol21@gmail.com>
2025-06-01 19:27:00 +0530

    ui changes

--- app/workspace/_components/WorkspaceHeader.jsx
+++ app/workspace/_components/WorkspaceHeader.jsx
@@ -4,8 +4,14 @@ import { FileText } from "lucide-react";
 import { UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
-
+import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 const WorkspaceHeader = ({ filename }) => {
+  const router = useRouter();
+  const goToDashboard = () => {
+    redirect("/dashboard");
+  }
+
   const saveButtonClicked = () => {
     toast("PDF saved successfully!");
   }
@@ -18,7 +24,8 @@ const WorkspaceHeader = ({ filename }) => {
       </h1>
       <h2 className="font-bold">{filename}</h2>
       <div className="flex items-center gap-4">
-         <Button className='cursor-pointer' onClick={saveButtonClicked}>Save</Button>
+         <Button className=' bg-blue-600 hover:bg-blue-700 cursor-pointer' onClick={goToDashboard}>Dashboard</Button>
+         <Button className=' bg-blue-600 hover:bg-blue-700 cursor-pointer' onClick={saveButtonClicked}>Save</Button>
         <UserButton
           appearance={{
             elements: {
